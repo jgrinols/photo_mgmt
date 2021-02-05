5632ca7080c2bf76c8042c068911f64486c9bef3
Justin Grinols <3200942+jgrinols@users.noreply.github.com>
2021-02-04 23:11:48 -0600

    alias photolibutils to fix up imports

--- utilities_pkg/setup.py
+++ utilities_pkg/setup.py
@@ -8,15 +8,15 @@ with open("requirements.txt") as f:
 
 setup(
     name='photolibutils',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(),
     setup_requires=["wheel"],
     install_requires=required,
     include_package_data=True,
     entry_points={
         "console_scripts": [
-            "pwgo-virtualfs = pwgo_virtualfs.virtualfs:entry",
-            "icloudpd = icloudpd.base:main"
+            "pwgo-virtualfs = photolibutils.pwgo_virtualfs.virtualfs:entry",
+            "icloudpd = photolibutils.icloudpd.base:main"
         ]
     }
 )
