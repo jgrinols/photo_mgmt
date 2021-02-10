ca8796ad8498a47b2146e473382ae28ec5bc1b75
Justin Grinols <3200942+jgrinols@users.noreply.github.com>
2021-02-09 22:38:37 -0600

    pull in bug fix from icloudpd

--- utilities_pkg/setup.py
+++ utilities_pkg/setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name='photolibutils',
-    version='0.3.5',
+    version='0.3.6',
     packages=find_packages(),
     setup_requires=["wheel"],
     install_requires=required,
