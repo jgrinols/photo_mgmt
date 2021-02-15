9e07a82aa80b0ee962ca341d4c0f7e35632c79be
Justin Grinols <3200942+jgrinols@users.noreply.github.com>
2021-02-15 09:13:05 -0600

    including exec script template for pwgo-autotag

--- utilities_pkg/setup.py
+++ utilities_pkg/setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name='photolibutils',
-    version='0.4.0',
+    version='0.4.1',
     packages=find_packages(),
     setup_requires=["wheel"],
     install_requires=required,
