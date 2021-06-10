3664378ae210e35d0dc3026f31a1a9d278aa08d1
Justin Grinols <3200942+jgrinols@users.noreply.github.com>
2021-06-10 14:56:26 +0000

    trying to get this deploy job conditional working

--- utilities_pkg/setup.py
+++ utilities_pkg/setup.py
@@ -11,7 +11,7 @@ with open("dev_requirements.txt") as f:
 
 setup(
     name='photolibutils',
-    version='0.6.0',
+    version='0.6.3',
     packages=find_packages(),
     setup_requires=['wheel'],
     install_requires=required,
