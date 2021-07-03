859e0074104d619155248ddbdb53e1ea6d9a1d58
Justin Grinols <3200942+jgrinols@users.noreply.github.com>
2021-07-03 00:16:02 +0000

    don't call rekognition if there's already results stored in db for the image

--- pwgo_helper_package/setup.py
+++ pwgo_helper_package/setup.py
@@ -16,7 +16,7 @@ with open(os.path.join(mod_path, "dev_requirements.txt")) as f:
 
 setup(
     name='pwgo_helper',
-    version='0.7.15',
+    version='0.7.16',
     packages=find_packages(),
     install_requires=required,
     extras_require={
