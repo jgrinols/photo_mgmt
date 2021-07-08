cb7a005145c62a8b935449668e3c18a5e560f506
Justin Grinols <3200942+jgrinols@users.noreply.github.com>
2021-07-08 19:10:08 +0000

    fix usage of child contest managers in metadata writer

--- pwgo_helper_package/pwgo_helper/agent/file_metadata_writer.py
+++ pwgo_helper_package/pwgo_helper/agent/file_metadata_writer.py
@@ -1,4 +1,6 @@
 """Container module for FileMetadataWriter"""
+from contextlib import ExitStack
+
 from pyexiv2 import ImageData
 
 from .pwgo_image import PiwigoImage
@@ -7,18 +9,20 @@ class FileMetadataWriter():
     """Synchronizes Piwigo image metadata from the database into exif/iptc fields in the physical file"""
     def __init__(self, img: PiwigoImage):
         self.image = img
+        self._exit_stack = None
         self._img_data = None
         self._img_file = None
 
     def __enter__(self):
-        self._img_file = self.image.open_file(mode='r+')
-        self._img_data = ImageData(self._img_file.read())
+        self._exit_stack = ExitStack()
+        self._exit_stack.__enter__()
+        self._img_file = self._exit_stack.enter_context(self.image.open_file(mode='r+'))
+        self._img_data = self._exit_stack.enter_context(ImageData(self._img_file.read()))
         self._img_file.seek(0)
         return self
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
-        self._img_data.close()
-        self._img_file.close()
+        self._exit_stack.__exit__(exc_type, exc_value, exc_traceback)
 
     def write(self) -> None:
         """Writes image metadata from Piwigo database to file
